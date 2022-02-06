7e52a6f96f1430a584abe9e31bc457852cf75f16
Filip Iran <iranf.03@spst.cz>
2022-02-06 16:25:45 +0100

    v1.0

--- Ukol3_metody_Csharp/Form1.Designer.cs
+++ Ukol3_metody_Csharp/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.vstup = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zjistit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vstup
+            // 
+            this.vstup.Location = new System.Drawing.Point(94, 12);
+            this.vstup.Name = "vstup";
+            this.vstup.Size = new System.Drawing.Size(100, 20);
+            this.vstup.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zadej řetězec:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(286, 43);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vstup);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Úkol 3 metody C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox vstup;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Ukol3_metody_Csharp/Form1.cs
+++ Ukol3_metody_Csharp/Form1.cs
@@ -16,5 +16,52 @@ namespace Ukol3_metody_Csharp
         {
             InitializeComponent();
         }
+
+        private void JeAlfanum (string vstup)
+        {
+            bool alfanum = true;
+            int mpismena = 0;
+            int vpismena = 0;
+            int cisla = 0;
+            int jznaky = 0;
+
+            for (int i = 0; i <= vstup.Length - 1; i++)
+            {
+                if (vstup[i] >=  '0' && vstup[i] <= '9')
+                {
+                    cisla++;
+                }
+
+                if (vstup[i] >= 'a' && vstup[i] <= 'z')
+                {
+                    mpismena++;
+                }
+
+                if (vstup[i] >= 'A' && vstup[i] <= 'Z')
+                {
+                    vpismena++;
+                }
+
+                if (vstup[i] < '0' || vstup[i] > '9' && vstup[i] < 'A' || vstup[i] > 'Z' && vstup[i] < 'a' || vstup[i] > 'z')
+                {
+                    jznaky++;
+                    alfanum = false;
+                }
+            }
+
+            if (alfanum == true)
+            {
+                MessageBox.Show("Řetězec je alfanumerický" + Environment.NewLine + "a obsahuje " + mpismena + " malých písmen, " + vpismena + " velkých písmen a " + cisla + " čísel.", "Výsledek");
+            }
+            else
+            {
+                MessageBox.Show("Řetězec není alfanumerický" + Environment.NewLine + "a obsahuje " + mpismena + " malých písmen, " + vpismena + " velkých písmen, " + cisla + " čísel a " + jznaky + " jiných znaků.", "Výsledek");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JeAlfanum(vstup.Text);
+        }
     }
 }
